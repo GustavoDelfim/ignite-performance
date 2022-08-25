89ab4863c3d1a448c9c5ade9b751b22dcc8ff2a7
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-08-25 08:28:14 -0300

    useMemo, useCallback and data formatted

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,17 +1,27 @@
+import { memo } from "react"
+
 export type ProductItem = {
   id: number
   price: number
   title: string
+  priceFormatted: string
 }
 
 interface ProductItemProps {
   product: ProductItem
+  onAddToWishList: (id: number) => void
 }
 
-export function ProductItem ({ product }: ProductItemProps) {
+function ProductItemComp ({ product, onAddToWishList }: ProductItemProps) {
   return (
     <div>
-      {product.title} - <strong>{product.price}</strong>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+
+      <button onClick={() => onAddToWishList(product.id)}>Add to wish list</button>
     </div>
   )
 }
+
+export const ProductItem = memo(ProductItemComp, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,21 +1,24 @@
 import { ProductItem } from "./ProductItem"
 
 interface SearchResultsProps {
-  results: {
-    id: number
-    price: number
-    title: string
-  }[]
+  results: ProductItem[]
+  totalPrice: number
+  onAddToWishList: (id: number) => void
 }
 
-export function SearchResults ({ results }: SearchResultsProps) {
+export function SearchResults ({ results, totalPrice, onAddToWishList }: SearchResultsProps) {
   return (
     <div>
+      <h2>
+        <> Total: {totalPrice} </>
+      </h2>
+      
       {results.map(item => {
         return (
           <ProductItem
             key={item.id}
             product={item}
+            onAddToWishList={onAddToWishList}
           />
         )
       })}
